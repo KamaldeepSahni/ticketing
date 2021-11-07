acf9bcb1d03f7459469587f35777f1abb898545c
Kamaldeep Sahni <ksahni687@gmail.com>
2021-11-07 13:30:52 +0530

    added startup log

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,6 +8,7 @@ import { natsWrapper } from './nats-wrapper';
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
+  console.log('Starting...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
